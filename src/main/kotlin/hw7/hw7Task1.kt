e63a886530557647a4121375986e1fa451f1341f
Mefodys <mefody123@gmail.com>
2022-11-26 20:11:12 +0100

    hw7 Task 1 fix

--- src/main/kotlin/hw7/hw7Task1.kt
+++ src/main/kotlin/hw7/hw7Task1.kt
@@ -6,57 +6,63 @@ package hw7
 // Также реализовать методы сложения и вычитания объектов.
 
 fun main() {
-    val newDrobnoeChislo1 = DrobnieChisla(1,2)
-    val newDrobnoeChislo2 = DrobnieChisla(3,5)
+    val newFractionalNumber1 = FractionalNumber(1,2)
+    val newFractionalNumber2 = FractionalNumber(3,8)
 
-    println("----")
-    slojenieUsingFunction(newDrobnoeChislo1,newDrobnoeChislo2)
-    vichitanieUsingFunction(newDrobnoeChislo1,newDrobnoeChislo2)
+    println("\nequals test:")
+    println(newFractionalNumber1.equals(newFractionalNumber1))
+    println(newFractionalNumber1.equals(newFractionalNumber2))
 
-    println("----")
-    println(newDrobnoeChislo1.slojenie(newDrobnoeChislo2))
-    println(newDrobnoeChislo1.vichitanie(newDrobnoeChislo2))
+    println("\ntoString test:")
+    println(newFractionalNumber1.toString())
 
+    println("\nhashCode test:")
+    println(newFractionalNumber1.hashCode())
+
+    println("\naddition test:")
+    newFractionalNumber1.addition(newFractionalNumber2)
+
+    println("\nsubstruction test:")
+    newFractionalNumber1.substruction(newFractionalNumber2)
 }
 
-//class DrobnieChisla(val chislitel:Int, val znamenatel:Int){
-//    init {
-//        println("DrobnoeChislo is created: $chislitel/$znamenatel")
-//    }
-//
-//}
-//
-fun slojenieUsingFunction (chislo1:DrobnieChisla,chislo2:DrobnieChisla){
-    println("${chislo1.chislitel * chislo2.znamenatel}/${chislo1.znamenatel * chislo2.znamenatel} + " +
-            "${chislo2.chislitel * chislo1.znamenatel}/${chislo2.znamenatel * chislo1.znamenatel} = " +
-            "${chislo1.chislitel * chislo2.znamenatel + chislo2.chislitel * chislo1.znamenatel}/${chislo2.znamenatel * chislo1.znamenatel}")
-
-}
-
-fun vichitanieUsingFunction (chislo1:DrobnieChisla,chislo2:DrobnieChisla){
-    println("${chislo1.chislitel * chislo2.znamenatel}/${chislo1.znamenatel * chislo2.znamenatel} - " +
-            "${chislo2.chislitel * chislo1.znamenatel}/${chislo2.znamenatel * chislo1.znamenatel} = " +
-            "${chislo1.chislitel * chislo2.znamenatel - chislo2.chislitel * chislo1.znamenatel}/${chislo2.znamenatel * chislo1.znamenatel}")
-
-}
-
-class DrobnieChisla(val chislitel:Int, val znamenatel:Int){
+class FractionalNumber(val numerator:Long, val denumerator:Long){
     init {
-        println("DrobnoeChislo is created: $chislitel/$znamenatel")
+        println("FractionalNumber is created: $numerator/$denumerator")
     }
 
-    fun slojenie (chislo2:DrobnieChisla): String{
-        val result= "${this.chislitel * chislo2.znamenatel}/${this.znamenatel * chislo2.znamenatel} + " +
-                "${chislo2.chislitel * this.znamenatel}/${chislo2.znamenatel * this.znamenatel} = " +
-                "${this.chislitel * chislo2.znamenatel + chislo2.chislitel * this.znamenatel}/${chislo2.znamenatel * this.znamenatel}"
+    fun addition (digit2:FractionalNumber): FractionalNumber{
+        val num = this.numerator * digit2.denumerator + digit2.numerator * this.denumerator
+        val denum = digit2.denumerator * this.denumerator
+        return FractionalNumber (num, denum)
+    }
+
+    fun substruction (digit2:FractionalNumber): FractionalNumber{
+        val num = this.numerator * digit2.denumerator - digit2.numerator * this.denumerator
+        val denum = digit2.denumerator * this.denumerator
+        return FractionalNumber (num, denum)
+    }
+
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as FractionalNumber
+
+        if (numerator != other.numerator) return false
+        if (denumerator != other.denumerator) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = numerator.toInt()
+        result = 31 * result + denumerator.toInt()
         return result
     }
 
-    fun vichitanie (chislo2:DrobnieChisla): String{
-        val result = ("${this.chislitel * chislo2.znamenatel}/${this.znamenatel * chislo2.znamenatel} - " +
-                "${chislo2.chislitel * this.znamenatel}/${chislo2.znamenatel * this.znamenatel} = " +
-                "${this.chislitel * chislo2.znamenatel - chislo2.chislitel * this.znamenatel}/${chislo2.znamenatel * this.znamenatel}")
-        return result
+    override fun toString(): String {
+        return "FractionalNumber(numerator=$numerator, denumerator=$denumerator)"
     }
 }
-

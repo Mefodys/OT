69199c4d436cc27431796f95153399632856384b
Mefodys <mefody123@gmail.com>
2022-12-14 12:06:00 +0100

    HW9 Line class refactor

--- src/main/kotlin/hw9/Line.kt
+++ src/main/kotlin/hw9/Line.kt
@@ -4,40 +4,34 @@ import kotlin.math.*
 
 class Line(val pointOne: Point, val pointTwo: Point) {
 
-    var firstPointX = pointOne.getX()
-    var firstPointY = pointOne.getY()
-    var secondPointX = pointTwo.getX()
-    var secondPointY = pointTwo.getY()
+    private var x1 = pointOne.getX()
+    private var y1 = pointOne.getY()
+    private var x2 = pointTwo.getX()
+    private var y2 = pointTwo.getY()
 
     constructor(firstPointX: Double, firstPointY: Double, secondPointX: Double, secondPointY: Double) :
             this(pointOne = Point(x = 0.0, y = 0.0), pointTwo = Point(x = 0.0, y = 0.0)) {
-        this.firstPointX = firstPointX
-        this.firstPointY = firstPointY
-        this.secondPointX = secondPointX
-        this.secondPointY = secondPointY
+        x1 = firstPointX
+        y1 = firstPointY
+        x2 = secondPointX
+        y2 = secondPointY
     }
 
     constructor(anotherLine: Line) : this(pointOne = Point(x = 0.0, y = 0.0), pointTwo = Point(x = 0.0, y = 0.0)) {
-        this.firstPointX = anotherLine.firstPointX
-        this.firstPointY = anotherLine.firstPointY
-        this.secondPointX = anotherLine.secondPointX
-        this.secondPointY = anotherLine.secondPointY
-
+        x1 = anotherLine.x1
+        y1 = anotherLine.y1
+        x2 = anotherLine.x2
+        y2 = anotherLine.y2
     }
 
     override fun toString(): String {
-        return "Line[Point[$firstPointX, $firstPointY], Point[$secondPointX, $secondPointY]]"
+        return "Line[Point[$x1, $y1], Point[$x2, $y2]]"
     }
 
     fun angle(): Double {
-        val x1 = this.firstPointX
-        val y1 = this.firstPointY
-        val x2 = this.secondPointX
-        val y2 = this.secondPointY
 
         val deltaX = (x2 - x1)
         val deltaY = (y2 - y1)
-
         val theta = (atan2(deltaY, deltaX)) / (PI / 180)
 
         if (theta < 0) return theta + 360
@@ -46,10 +40,6 @@ class Line(val pointOne: Point, val pointTwo: Point) {
 
     //    Евклидова длина
     fun len(): Double {
-        val x1 = this.firstPointX
-        val y1 = this.firstPointY
-        val x2 = this.secondPointX
-        val y2 = this.secondPointY
 
         return sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1))
     }
@@ -60,26 +50,16 @@ class Line(val pointOne: Point, val pointTwo: Point) {
     //Xnew = Xold x cosθ – Yold x sinθ
     //Ynew = Xold x sinθ + Yold x cosθ
 
-
     fun rotate(degrees: Double): Line {
-        val x1 = this.firstPointX
-        val y1 = this.firstPointY
-        val x2 = this.secondPointX
-        val y2 = this.secondPointY
 
         //Angle in Degrees* PI = Angle in Radians × 180°/π
         val rad = degrees * PI / 180
 
-//        val shiftToZeroX1 =
-//        val shiftToZeroY1 =
-
-        val cos_angle = cos(rad);
-        val sin_angle = sin(rad);
-
-//        val new_bx = a(:)+[cos_angle -sin_angle;
-//        val new_by = sin_angle cos_angle]*(b-a).';
-
-        //если одна из точек не находится в 0.0, то сместить всю линию к 0.0
+        // val cos_angle = cos(rad);
+        // val sin_angle = sin(rad);
+        // val new_bx = a(:)+[cos_angle -sin_angle;
+        // val new_by = sin_angle cos_angle]*(b-a).';
+        // ??? если одна из точек не находится в 0.0, то сместить всю линию к 0.0
 
         val Px2 = (x2) * cos(rad) + (y2) * sin(rad)
         val Py2 = (x2) * sin(rad) - (y2) * cos(rad)
@@ -92,41 +72,36 @@ class Line(val pointOne: Point, val pointTwo: Point) {
 
         return Line(x1, y1, roundedPx2, roundedPy2)
 
+//        val x2 = (x2 * cos(degrees) - y2 * sin(degrees))
+//        val y2 = (y2 * cos(degrees) + x2 * sin(degrees))
+//        return Line(x1, y1, x2, y2)
     }
 
     fun rotate90(dir: Int): Line {
-        val x1 = this.firstPointX
-        val y1 = this.firstPointY
-        val x2 = this.secondPointX
-        val y2 = this.secondPointY
 
         val rad: Double
+        val deg: Double
+
         when {
+
             dir >= 0 -> {
                 rad = 90 * PI / 180
+                deg = 180 * rad / PI
             }
 
             else -> {
                 rad = -90 * PI / 180
+                deg = 180 * rad / PI
             }
+
         }
 
-        val Px = (x2 * cos(rad) + y2 * sin(rad)) + x1
-        val Py = (x2 * sin(rad) - y2 * cos(rad)) + y2
-
-        val rounded1: Double = String.format("%.2f", Px).toDouble()
-        val rounded2: Double = String.format("%.2f", Py).toDouble()
-
-        return Line(x1, y1, rounded1, rounded2)
+        return rotate(deg)
 
     }
 
     //  Проверяет лежит ли точка на линии
     fun isOnLine(a: Point): Boolean {
-        val x1 = this.firstPointX
-        val y1 = this.firstPointY
-        val x2 = this.secondPointX
-        val y2 = this.secondPointY
 
         val x = a.getX()
         val y = a.getY()
@@ -139,18 +114,6 @@ class Line(val pointOne: Point, val pointTwo: Point) {
 
     //    Проверяет лежит ли точка на линии
     fun isOnLine(x: Double, y: Double): Boolean {
-        val x1 = this.firstPointX
-        val y1 = this.firstPointY
-        val x2 = this.secondPointX
-        val y2 = this.secondPointY
-
-        val x = x
-        val y = x
-
-        return when ((x - x1) / (x2 - x1) == (y - y1) / (y2 - y1)) {
-            true -> true
-            false -> false
-        }
+        return isOnLine(Point(x, y))
     }
-
 }
